6a870980b02b3853db499c5eefff0f9daae33683
truizdiaz <tomiruizdiaz@gmail.com>
2020-08-27 19:37:52 -0300

    Generando Migracion a MySQL y SQL Server

---
+++ BackEnd/Migrations/20200827223202_Inicial.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BackEnd.Migrations
+{
+    public partial class Inicial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Usuario",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    NombreUsuario = table.Column<string>(type: "varchar(20)", nullable: false),
+                    Password = table.Column<string>(type: "varchar(50)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Usuario", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Usuario");
+        }
+    }
+}
--- BackEnd/Persistence/Context/AplicationDbContext.cs
+++ BackEnd/Persistence/Context/AplicationDbContext.cs
@@ -1,11 +1,18 @@
-﻿using System;
+﻿using BackEnd.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace BackEnd.Persistence.Context
 {
-    public class AplicationDbContext
+    public class AplicationDbContext: DbContext
     {
+        public DbSet<Usuario> Usuario { get; set; }
+        public AplicationDbContext(DbContextOptions<AplicationDbContext> options): base(options)
+        {
+
+        }
     }
 }

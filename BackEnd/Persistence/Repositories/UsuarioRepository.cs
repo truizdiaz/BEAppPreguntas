2ae41509be4a3be5a20bf78f0027c88eba9196d8
truizdiaz <tomiruizdiaz@gmail.com>
2020-08-28 14:32:26 -0300

    SaveUser IUsuarioRepository, UsuarioRepository

--- BackEnd/Persistence/Repositories/UsuarioRepository.cs
+++ BackEnd/Persistence/Repositories/UsuarioRepository.cs
@@ -1,11 +1,25 @@
-﻿using System;
+﻿using BackEnd.Domain.IRepositories;
+using BackEnd.Domain.Models;
+using BackEnd.Persistence.Context;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace BackEnd.Persistence.Repositories
 {
-    public class UsuarioRepository
+    public class UsuarioRepository: IUsuarioRepository
     {
+        private readonly AplicationDbContext _context;
+        public UsuarioRepository(AplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task SaveUser(Usuario usuario) 
+        {
+            _context.Add(usuario);
+            await _context.SaveChangesAsync();
+        }
     }
 }

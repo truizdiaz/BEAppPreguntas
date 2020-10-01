5c65c19a372baa6e99a6c0343788a1f9a104274d
truizdiaz <tomiruizdiaz@gmail.com>
2020-10-01 11:51:15 -0300

    Creacion de interfaces, servicios y repositorio (Cuestionario)

---
+++ BackEnd/Persistence/Repositories/CuestionarioRepository.cs
@@ -0,0 +1,25 @@
+﻿using BackEnd.Domain.IRepositories;
+using BackEnd.Domain.Models;
+using BackEnd.Persistence.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BackEnd.Persistence.Repositories
+{
+    public class CuestionarioRepository: ICuestionarioRepository
+    {
+        private readonly AplicationDbContext _context;
+        public CuestionarioRepository(AplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task CreateCuestionario(Cuestionario cuestionario)
+        {
+            _context.Add(cuestionario);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
